60068da0ace4d3f3edceb45c11ca033af88b6dd2
Emilio J. Franco <ejfranco06@gmail.com>
2018-07-29 19:16:02 -0400

    Add player crossing condition and lives

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ class Enemy {
 
     if(this.checkCollision()) {
       player.resetPlayer();
+      player.decreaseLives();
     }
 
   }
@@ -66,20 +67,24 @@ class Enemy {
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-  constructor(x, y) {
-    this.startingX = x;
-    this.startingY = y;
-    this.x = x;
-    this.y = y;
+  constructor() {
+    this.startingX = 202;
+    this.startingY = 395;
     this.xStep = 101;
     this.yStep = 83;
+    this.crossed = 0;
+    this.lives = 3;
+    this.x = this.startingX;
+    this.y = this.startingY;
+
     this.sprite = 'images/char-boy.png';
   }
 
   update() {
-    // if (this.checkCollisions()) {
-    //   this.resetPlayer();
-    // }
+    if( this._reachedWater() ) {
+      this._increaseCrossed();
+      this.resetPlayer();
+    }
 
   }
 
@@ -122,44 +127,38 @@ class Player {
     }
   }
 
+  increaseLives() {
+    this.lives++;
+  }
+
+  decreaseLives() {
+    this.lives--;
+  }
+  _increaseCrossed() {
+    this.crossed++;
+  }
+  _reachedWater() {
+    return this.y === -20;
+  }
   resetPlayer() {
     this.x = this.startingX;
     this.y = this.startingY;
   }
 
-  checkCollisions() {
-    let collision = false;
-    allEnemies.forEach( enemy => {
-      if(this._sameRow(enemy) && this._isWithinHitbox(enemy)) {
-        collision = true;
-      }
-    });
-    return collision;
-  }
-
-  _isWithinHitbox(enemy) {
-    const leftHitBox = player.x - 65;
-    const rightHitBox = player.x + 65;
-    return enemy.x > leftHitBox && enemy.x < rightHitBox;
-  }
-
-  _sameRow(enemy) {
-    return enemy.y === player.y;
-  }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const enemy1 = new Enemy(0, 63, 200);
-const enemy2 = new Enemy(160, 146, 250);
+const enemy2 = new Enemy(260, 146, 250);
 const enemy3 = new Enemy(80, 229, 230);
 
 
 allEnemies.push(enemy1, enemy2, enemy3);
 
 
-const player = new Player(202, 395);
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
