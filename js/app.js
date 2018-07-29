3189c88a5bd04e0e616d8e600a4f8a8ea6f47490
Emilio J. Franco <ejfranco06@gmail.com>
2018-07-29 07:33:08 -0400

    Re-factor Enemy class

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
 const allEnemies = [];
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    constructor(x, y){
+      this.x = x;
+      this.y = y;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-  
-};
+      // The image/sprite for our enemies, this uses
+      // a helper we've provided to easily load images
+      this.sprite = 'images/enemy-bug.png';
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+
+    };
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+}
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -51,7 +57,7 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const enemy1 = new Enemy(0,60);
-const enemy2 = new Enemy(0,15 0);
+const enemy2 = new Enemy(0,150);
 const enemy3 = new Enemy(0,250);
 
 allEnemies.push(enemy1, enemy2, enemy3);
